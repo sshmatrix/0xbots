b8ad854be3bf5d4b3ba8c4a5fb72c25aa43c08bb
sshmatrix <cryptosauron@protonmail.com>
2022-04-23 10:38:07 +0200

    SIWE workflow backend

---
+++ 0xAUTH/frontend/siwe/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  resolve: {
+    fallback: {
+      fs: false,
+      path: false,
+      util: false
+    }
+  },
+  output: {
+    filename: 'main.js',
+    path: path.join(__dirname,'/../../public_html/frontend')
+  },
+  externals: {
+    'Config': JSON.stringify(process.env.NODE_ENV === 'production' ? {
+      serverUrl: "https://inpl.one:3001"
+    } : {
+      serverUrl: "http://localhost:3000"
+    })
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      path: path.join(__dirname,'/../../public_html/frontend')
+    })
+  ]
+};
